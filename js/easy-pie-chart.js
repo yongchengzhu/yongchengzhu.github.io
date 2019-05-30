b0b577527b6266962defc9223e81ff2049216f66
yongchengzhu <yongchengzhu002@gmail.com>
2019-05-29 21:36:40 -0400

    Added dots to owl carousel. And render it whenever user is in view of the pie charts.

--- js/easy-pie-chart.js
+++ js/easy-pie-chart.js
@@ -1,7 +1,31 @@
-$('.chart').easyPieChart({
-  barColor: "#fff",
-  trackColor: false,
-  scaleColor: false,
-  lineWidth: 6,
-  size: 152
-})
+//
+// Offset Variables
+//
+const itemTopOffset          = $('.item').offset().top;
+let   viewPortBottomOffset   = window.pageYOffset + window.innerHeight;
+
+//
+// Main Logics
+//
+renderPieCharts(viewPortBottomOffset, itemTopOffset);
+
+$(window).on('scroll', () => {
+  const viewPortBottomOffset = window.pageYOffset + window.innerHeight;
+
+  renderPieCharts(viewPortBottomOffset, itemTopOffset);
+});
+
+//
+// Render Functions
+//
+function renderPieCharts(viewPortBottomOffset, itemTopOffset) {
+  if (viewPortBottomOffset > itemTopOffset) {
+    $('.chart').easyPieChart({
+      barColor: "#fff",
+      trackColor: false,
+      scaleColor: false,
+      lineWidth: 6,
+      size: 152
+    });
+  }
+}
--- js/owl-carousel.js
+++ js/owl-carousel.js
@@ -1,3 +1,16 @@
 $('.owl-carousel').owlCarousel({
-  loop: false
+  loop: false,
+  nav: true,
+  dots: true,
+  responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+  }
 });
