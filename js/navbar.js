2265006d5d85dcf164ffcfe226f7cbf5c1e272db
yongchengzhu <yongchengzhu002@gmail.com>
2019-07-06 23:01:57 -0400

    Fixed synchronous bug for navbar load.

--- js/navbar.js
+++ js/navbar.js
@@ -1,37 +1,39 @@
-const navbarTopOffset = document.querySelector('.navbar').offsetTop;
-let windowTopOffset = window.pageYOffset;
-
-setFixedTop(windowTopOffset, navbarTopOffset)
-
-window.addEventListener("scroll", () => {
-  windowTopOffset = window.pageYOffset;
+$('#navbar').load('components/navbar.html', function() {
+  const navbarTopOffset = document.querySelector('.navbar').offsetTop;
+  let windowTopOffset = window.pageYOffset;
 
   setFixedTop(windowTopOffset, navbarTopOffset)
-});
 
-function setFixedTop(windowTopOffset, navbarTopOffset) {
-  if (windowTopOffset >= navbarTopOffset) {
-    document.querySelector('.navbar').classList.add("fixed-top");
-  } else {
-    document.querySelector('.navbar').classList.remove("fixed-top");
-  }
-};
+  window.addEventListener("scroll", () => {
+    windowTopOffset = window.pageYOffset;
 
-const navlinkNodeList = document.querySelectorAll('a.nav-scroll');
-
-navlinkNodeList.forEach(navlink => {
-  navlink.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const targetId  = navlink.getAttribute('href');
-    const targetPos = document.querySelector(targetId).offsetTop;
-    const navbarHeight = document.querySelector('.navbar').clientHeight;
-    const scrollPos = targetPos - navbarHeight;
-
-    window.scroll({
-      top: scrollPos,
-      left: 0,
-      behavior: 'smooth'
-    })
+    setFixedTop(windowTopOffset, navbarTopOffset)
   });
+
+  function setFixedTop(windowTopOffset, navbarTopOffset) {
+    if (windowTopOffset >= navbarTopOffset) {
+      document.querySelector('.navbar').classList.add("fixed-top");
+    } else {
+      document.querySelector('.navbar').classList.remove("fixed-top");
+    }
+  };
+
+  const navlinkNodeList = document.querySelectorAll('a.nav-scroll');
+
+  navlinkNodeList.forEach(navlink => {
+    navlink.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const targetId  = navlink.getAttribute('href');
+      const targetPos = document.querySelector(targetId).offsetTop;
+      const navbarHeight = document.querySelector('.navbar').clientHeight;
+      const scrollPos = targetPos - navbarHeight;
+
+      window.scroll({
+        top: scrollPos,
+        left: 0,
+        behavior: 'smooth'
+      })
+    });
+  });  
 });
